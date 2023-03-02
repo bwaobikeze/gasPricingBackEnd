ecdd6a55a516649ece06c869ce1709ff7787e6b8
bwaobikeze <bwaobikeze@my.lonestar.edu>
2023-03-02 15:41:41 -0600

    Intilizing the QuoteModule Class

---
+++ FuelQuoteModule.js
@@ -0,0 +1,13 @@
+class fuelQuoteModule {
+  //Takes the city input from the user and in theory return the current lowest PPG(price per gallon) returns PPG
+  UCLocationOC() {}
+
+  //add a quote to the users account(enter object and store in a Linked List the new quote)
+  UCClienQuoteManagement() {}
+
+  //displays quotes in a tabular display(this should return data in a format that the front end team can display anyway they like.
+  UCClientHistory() {}
+
+  //what profit margin we want calculate the quote for the user
+  UCPricingMargin() {}
+}
