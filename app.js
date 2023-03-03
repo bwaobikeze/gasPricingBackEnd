cee183920daec639a433271ab7b3871577e8c9dd
bwaobikeze <bwaobikeze@my.lonestar.edu>
2023-03-03 11:17:48 -0600

    Added UserList to Test fuelQuoteModule class

--- app.js
+++ app.js
@@ -1,6 +1,45 @@
 //var fuelQuoteModule = require("./fuelQuoteModules");
+
+const { Console } = require("console");
+
 //testing out with test user objects
 ListOfUsers = [
-  { name: "bob", Age: 19, Adress: "8352 Redwood St Plano TX 75074" },
-  { name: "Brian", Age: 19, Adress: "11 Illinois Dr Fort Worth TX 76116" },
+  {
+    email: "notaprguy@msn.com",
+    Fullname: "Bob Igar",
+    Age: 19,
+    Adress1: "8352 Redwood St",
+    Adress2: "",
+    City: "Plano",
+    State: "TX",
+    Zipcode: "75074",
+    UserHistory: [],
+  },
+  {
+    email: "roesch@gmail.com",
+    name: "Rose Wild",
+    Age: 19,
+    Adress1: "11 Illinois Dr",
+    Adress2: "",
+    City: "Fort Worth",
+    State: "TX",
+    Zipcode: "76116",
+  },
 ];
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+//simulating logging in
+readline.question("Please Provide Email: ", (userEmail) => {
+  let returnedUser;
+  for (let i = 0; i < ListOfUsers.length; i++) {
+    if (userEmail == ListOfUsers[i].email) {
+      returnedUser = ListOfUsers[i];
+      break;
+    }
+  }
+  console.log(returnedUser);
+
+  readline.close();
+});
--- fuelQuoteModule.js
+++ fuelQuoteModule.js
@@ -3,7 +3,7 @@ var quote = require("./quotes");
 class fuelQuoteModule {
   differntquotes = { Houston: 2.99, Auston: 6.99, "san antonio": 1.99 };
   //Takes the city input from the user and in theory return the current lowest PPG(price per gallon) returns PPG
-  UCLocationOC(gallons, city) {}
+  UCLocationOC(gallonsRequested, CityDropDown, UserEmail) {}
 
   //add a quote to the users account(enter object and store in a Linked List the new quote)
   UCClienQuoteManagement() {}
